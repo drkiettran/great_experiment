b49340909f8b69a2cb2b52ab79bbcb3f439240b4
drkiettran <38035597+drkiet@users.noreply.github.com>
2023-08-29 07:55:36 -0400

    added creating queue scenario

--- bdd/features/activemq_login.feature
+++ bdd/features/activemq_login.feature
@@ -6,3 +6,9 @@ Given I am an ActiveMQ web console "http://localhost:8161"
 When I login using user "admin" and password "admin"
 Then I should be on the main website
 
+@wip
+Scenario: Create a queue
+Given I am an ActiveMQ web console "http://localhost:8161"
+And I login using user "admin" and password "admin"
+When I create a queue "my_queue"
+Then I should see "my_queue" in the active queue list
--- bdd/features/steps/activemq_steps.py
+++ bdd/features/steps/activemq_steps.py
@@ -24,6 +24,7 @@ def step_impl(context, website_url):
 
 
 @when(u'I login using user "{user}" and password "{password}"')
+@given(u'I login using user "{user}" and password "{password}"')
 def step_impl(context, user, password):
     url = context.activemq_website_url.replace("://", f'://{user}:{password}@')
     print(f'\t** I login to {url}')
@@ -42,4 +43,54 @@ def step_impl(context):
 
     assert_that(text, equal_to("Welcome to the Apache ActiveMQ!"))
     demo()
+
+@when(u'I create a queue "{queue_name}"')
+def step_impl(context, queue_name):
+    context.queue_name = queue_name
+    # go to manage activeMQ broker
+    elems = context.driver.find_elements(By.XPATH, "//a[@title='Manage ActiveMQ broker']")
+    if not elems:
+        assert(False)
+        return
+    elems[0].click()
+
+    # go to Queues page
+    elems = context.driver.find_elements(By.XPATH, "//a[@title='Queues']")
+    if not elems:
+        assert(False)
+        return
+    elems[0].click()
+
+    # enter queue name
+    elems = context.driver.find_elements(By.XPATH, "//input[@name='JMSDestination']")
+    if not elems:
+        assert(False)
+        return    
+    elems[0].send_keys(queue_name)
     
+    # click on Create button
+    elems = context.driver.find_elements(By.XPATH, "//input[@type='submit' and @value='Create']")
+    if not elems:
+        assert(False)
+        return
+    elems[0].click()
+
+    demo()    
+    
+
+@then(u'I should see "{queue_name}" in the active queue list')
+def step_impl(context, queue_name):
+    elems = context.driver.find_elements(By.XPATH, f"//td/a[contains(text(),'{queue_name}')]")
+    if not elems:
+        assert(False)
+        return
+    text = elems[0].text
+    assert_that(text, equal_to(queue_name))
+
+    # search and remove the queue we just created as a part of cleanup activities
+    elems = context.driver.find_elements(By.XPATH, 
+                                         "//td/a[contains(text(),'my_queue')]/../../td[7]/a[contains(text(),'Delete')]")
+    if not elems:
+        assert(False)
+        return
+    elems[0].click()
